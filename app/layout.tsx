7d33047af8d790b2b4f14e8bc88cf4f6ef8b0ec1
AbdulRahman Maniar <ar1vuog@gmail.com>
2025-07-22 08:17:11 +0000

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Terminated | Falak',
+  description: 'Website Terminated',
+  generator: 'falak.me',
 }
 
 export default function RootLayout({
